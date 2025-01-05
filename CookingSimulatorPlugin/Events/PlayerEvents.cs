104f5126ce8bfe1c665d59cc5eddc456d0406202
sky3z <hensseddev@gmail.com>
2025-01-05 03:14:34 +0300

    fix

--- CookingSimulatorPlugin/Events/PlayerEvents.cs
+++ CookingSimulatorPlugin/Events/PlayerEvents.cs
@@ -27,6 +27,42 @@ namespace CookingSimulatorPlugin.Events
                     ev.Player.Broadcast(3, $"Вы взяли булку!", Broadcast.BroadcastFlags.Normal, true);
                     break;
 
+                case "TakeApple":
+                    if (!Plugin.Storage.Add(ev.Player, new Apple()))
+                    {
+                        ev.Player.Broadcast(3, $"Яблоко уже имеется у вас в инвентаре!", Broadcast.BroadcastFlags.Normal, true);
+                        break;
+                    }
+                    ev.Player.Broadcast(3, $"Вы взяли яблоко!", Broadcast.BroadcastFlags.Normal, true);
+                    break;
+
+                case "TakeLettuce":
+                    if (!Plugin.Storage.Add(ev.Player, new Lettuce()))
+                    {
+                        ev.Player.Broadcast(3, $"Лист салата уже имеется у вас в инвентаре!", Broadcast.BroadcastFlags.Normal, true);
+                        break;
+                    }
+                    ev.Player.Broadcast(3, $"Вы взяли лист салата!", Broadcast.BroadcastFlags.Normal, true);
+                    break;
+
+                case "TakeOliveOil":
+                    if (!Plugin.Storage.Add(ev.Player, new OliveOil()))
+                    {
+                        ev.Player.Broadcast(3, $"Оливковое масло уже имеется у вас в инвентаре!", Broadcast.BroadcastFlags.Normal, true);
+                        break;
+                    }
+                    ev.Player.Broadcast(3, $"Вы взяли оливковое масло!", Broadcast.BroadcastFlags.Normal, true);
+                    break;
+
+                case "TakeTomato":
+                    if (!Plugin.Storage.Add(ev.Player, new Tomato()))
+                    {
+                        ev.Player.Broadcast(3, $"Помидор уже имеется у вас в инвентаре!", Broadcast.BroadcastFlags.Normal, true);
+                        break;
+                    }
+                    ev.Player.Broadcast(3, $"Вы взяли помидор!", Broadcast.BroadcastFlags.Normal, true);
+                    break;
+
                 case "FryPickup":
                     Plugin.Storage.Fry(ev.Player);
                     break;
